c61f0234e7e0ac918fd9bd942a89c1dd6ba35ec9
Gabriel Moreno <gantoreno@gmail.com>
2022-09-23 14:07:43 -0400

    Add `viewport` meta tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import DarkModeProvider from "../providers/DarkModeProvider"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <DarkModeProvider>
-      <Component {...pageProps} />
-    </DarkModeProvider>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <DarkModeProvider>
+        <Component {...pageProps} />
+      </DarkModeProvider>
+    </>
   )
 }
 
