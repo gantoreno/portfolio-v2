cecc50603318493d3dbb1d6c8a4646bca2c4cacc
Gabriel Moreno <gabriel.moreno@zemoga.com>
2023-05-25 11:35:08 -0400

    Inline logo code

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image"
 import { default as NextLink } from "next/link"
 import { BiMoon, BiSun } from "react-icons/bi"
 
@@ -9,19 +8,25 @@ import Link from "../link/Link"
 
 import styles from "./Navbar.module.css"
 
-import logo from "../../assets/svg/logo.svg"
-
 const Logo: React.FC = () => {
   return (
     <NextLink href="/" className={styles.home}>
       <a aria-label="Home">
-        <Image
-          src={logo}
+        <svg
           className={styles.logo}
-          width={24}
-          height={48}
-          alt="Logo"
-        />
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 50 100"
+        >
+          <g id="Layer_2" data-name="Layer 2">
+            <g id="Layer_1-2" data-name="Layer 1">
+              <g id="Layer_2-2" data-name="Layer 2">
+                <g id="Layer_1-2-2" data-name="Layer 1-2">
+                  <path d="M50,37.5a25,25,0,0,0-6.62-16.95A18.74,18.74,0,0,0,50,6.25a6.25,6.25,0,0,0-12.5,0,6.25,6.25,0,0,1-6.25,6.25H25a25,25,0,0,0,0,50A12.5,12.5,0,1,1,12.5,75,6.25,6.25,0,0,0,0,75,25,25,0,1,0,41.53,56.25,24.92,24.92,0,0,0,50,37.5Zm-37.5,0A12.5,12.5,0,1,1,25,50,12.5,12.5,0,0,1,12.5,37.5Z" />
+                </g>
+              </g>
+            </g>
+          </g>
+        </svg>
       </a>
     </NextLink>
   )
